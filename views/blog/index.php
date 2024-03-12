5c7f1a632965685c41a16cfbf84c16cb3d9058ef
tibix19 <tibix19@gmail.com>
2024-03-12 21:12:48 +0100

    update uikit 1.3 + logo

--- views/blog/index.php
+++ views/blog/index.php
@@ -1,20 +1,28 @@
- <h1>Les derniers articles</h1>
+ <h1 style="font-family: Helvetica;">Les derniers articles</h1>
+
+ <?php if(!empty($params['message'])): ?>
+     <div class="uk-alert-warning" uk-alert>
+         <a class="uk-alert-close" uk-close></a>
+         <p><?php echo $params['message']; ?></p>
+     </div>
+ <?php endif; ?>
+
 
  <div class="uk-container uk-container-expand uk-container-content">
      <div class="uk-child-width-1-2@m uk-grid-match uk-grid-small" uk-grid>
          <?php foreach ($params['posts'] as $post): ?>
              <div>
                  <div class="uk-card uk-card-default uk-card-hover uk-margin-bottom">
-                     <div class="uk-card-body">
-                         <h2><?= $post->title ?></h2>
+                     <div class="uk-card-body" >
+                         <h2 class="uk-card-title"><?= $post->title ?></h2>
                          <div>
                              <?php foreach($post->getTags() as $tag): ?>
                                  <span class="uk-badge uk-margin-small-right"><a href="/tags/<?= $tag->id ?>" class="uk-link-muted uk-text-emphasis"><?= $tag->name ?></a></span>
                              <?php endforeach; ?>
                          </div>
-                         <small class="uk-text-meta">Publié le <?= $post->getCreatedAt() ?></small>
-                         <small class="uk-text-meta">par <?= $post->getCreatorPost(); ?></small>
                          <p><?= $post->getExcerpt() ?></p>
+                         <small class="uk-text-meta">Publié le <?= $post->getCreatedAt() ?></small>
+                         <small class="uk-text-meta">par <?= $post->getCreatorPost(); ?></small> <br><br>
                          <?= $post->getButton() ?>
                      </div>
                  </div>
--- views/layout.php
+++ views/layout.php
@@ -9,10 +9,13 @@
     <script src="../../../node_modules/uikit/dist/js/uikit-icons.min.js"></script>
 </head>
 <body>
-<div class="uk-container uk-container-expand uk-padding">
+<div class="uk-container uk-container-expand ">
     <nav class="uk-navbar-container uk-navbar-transparent" uk-navbar>
         <div class="uk-navbar-left">
-            <a class="uk-navbar-item uk-logo" href="/">Log Your Knowledge</a>
+            <a class="uk-navbar-item uk-logo" href="/">
+                <img src="../../../public/logo/logo.png" width="260" height="160" alt="Log Your Knowledge logo">
+            </a>
+            <a class="uk-navbar-item uk-logo" href="/"></a>
             <div class="uk-navbar-item">
                 <form action="/posts" method="GET">
                     <input class="uk-input" type="search" name="search" placeholder="Rechercher...">
@@ -24,8 +27,8 @@
             <ul class="uk-navbar-nav">
                 <?php // affiche le lien pour accéder à la page admin et le bouton pour se deco si connecter
                 if(isset($_SESSION['authAdmin']) &&  $_SESSION['authAdmin'] == 1 ): ?>
-                    <li><a href="/create">Écrire un article</a></li>
-                    <button class="uk-button uk-button-default" type="button">Menu</button>
+                    <li><a href="/create"><span uk-icon="file-edit"></span>Write</a></li>
+                    <button class="uk-button uk-button-link" uk-icon="icon: user" type="button"></button>
                     <div uk-dropdown="mode: click">
                         <ul class="uk-nav uk-dropdown-nav">
                             <li class="uk-active"><a href="/account">Profil</a></li>
@@ -41,19 +44,18 @@
                     </div>
                 <?php // affiche le lien pour se connecter si pas connecter, mais pour les users standard seulement ces liens vont s'afficher
                 elseif(isset($_SESSION['authAdmin']) == 2): ?>
-                    <li><a href="/create">Écrire un article</a></li>
+                    <li><a href="/create"><span uk-icon="file-edit"></span>Write</a></li>
                     <li><a href="/myposts">Voir mes postes</a></li>
                     <li><a href="/account">Account</a></li>
                     <li><a href="/logout">Se déconnecter</a></li>
                 <?php // affiche le lien pour se connecter si pas connecter
                 else: // si pas connecter on afficher la page pour se co ?>
-                    <li><a href="/create">Écrire un article</a></li>
-                    <li><a href="/account" ">Se connecter</a></li>
+                    <li><a href="/create"><span uk-icon="file-edit"></span>Write</a></li>
+                    <li><a href="/account">Se connecter</a></li>
                 <?php endif; ?>
             </ul>
         </div>
     </nav >
-    <hr class="uk-divider-icon">
     <div class="uk-grid uk-child-width-1-1 uk-margin-small-top">
         <div>
             <?= $content ?>
